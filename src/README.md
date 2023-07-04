254dfba0fd4c9fc2008f5d7177c345cbb9619a79
Felipe Daniel <felipe.dandev@gmail.com>
2023-07-04 00:00:07 -0300

    first commit on vuejs with laravel api

---
+++ src/README.md
@@ -0,0 +1,5 @@
+# vue3 composition API
+
+It is personal studies of vuejs to consume res API 
+
+On this example , we are consume a Rest API maked on Laravel
--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,3 +1,35 @@
 <template>
     <h1>Lista de Tarefas</h1>
+    <ul>
+        <li v-for="todo in todos" :key="todo.identify">
+            {{ todo.title }}
+        </li>
+    </ul>
+    <input type="text" v-model="name">
 </template>
+
+<script>
+import { onMounted, ref } from 'vue'
+import TodoService from '@/services/todos.service'
+
+export default {
+    name: 'Todos', 
+    setup() {
+        const todos = ref([])
+
+        onMounted(() => {
+            TodoService.getAll()
+                  .then(response => {
+                    console.log(response)
+                    todos.value = response.data.data
+                  })
+                  .catch(error => console.log(error))
+        })
+
+        return {
+            todos
+        }
+       
+    }
+}
+</script>
