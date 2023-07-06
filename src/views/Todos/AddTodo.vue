f87f14c268a175aafef55ccc8ec6992a7a933983
Felipe Daniel <felipe.dandev@gmail.com>
2023-07-06 00:37:05 -0300

    add todos

---
+++ src/views/Todos/AddTodo.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>Adicionar Nova Tarefa</h1>
+
+        <form action="#" method="POST" @submit.prevent="addTodo">
+            <input type="text" name="title" placeholder="Título" v-model="todo.name">
+            <input type="text" name="description" placeholder="Descrição" v-model="todo.description">
+            <button type="submit" :disabled="todo.loading">
+                <span v-if="todo.loading">Enviando ...</span>
+                <span v-else>Enviar</span>
+                Enviar
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+import TodoService from '@/services/todos.service'
+import router from '@/router'
+
+export default {
+    name: 'AddTodo', 
+
+    setup() {
+
+        // const name = ref('')
+        // const description = ref('')
+
+        const todo = reactive({
+            name: '', 
+            description: '', 
+            completed: false, 
+            loading: false
+        })
+
+        const addTodo = () => {       
+            todo.loading = true     
+            TodoService.addTodo({...todo})
+                       .then(() => router.push({name: 'todos.index'}))
+                       .finally(() => todo.loading = false)
+        }
+
+        return {
+            addTodo, 
+            todo
+        }
+    }
+}
+</script>
--- src/views/Todos/Todos.vue
+++ src/views/Todos/Todos.vue
@@ -1,5 +1,11 @@
 <template>
-    <h1>Lista de Tarefas</h1>
+    <h1>
+        Lista de Tarefas
+        <router-link :to="{name: 'todos.create'}">+</router-link>
+    </h1>
+
+    <div v-show="loading">Carregando ... </div>
+
     <ul>
         <li v-for="todo in todos" :key="todo.identify">
             {{ todo.title }}
@@ -17,16 +23,22 @@ export default {
     setup() {
         const todos = ref([])
 
+        const loading = ref(false)
+
         onMounted(() => {
+            loading.value = true
+
             TodoService.getAll()
                   .then(response => {
                     console.log(response)
                     todos.value = response.data.data
                   })
                   .catch(error => console.log(error))
+                  .finally(() => loading.value = false)
         })
 
         return {
+            loading, 
             todos
         }
        
